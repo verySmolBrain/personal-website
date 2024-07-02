49fcd54a4451449ccbf497320a27215fbc533cf6
smolbrain <randomlyrandamu@gmail.com>
2024-07-03 04:37:59 +1000

    init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-## Personal Website
+# 🔥 Features (Todo)
+
+- Type-safe markdown
+- Accessible (Keyboard / VoiceOver)
+- Responsive (mobile ~ desktop)
+- SEO-Friendly
+- Light & Dark Mode
+- Fuzzy Search
+- Sitemap && RSS Feed
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), react(), sitemap()]
+});
